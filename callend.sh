60288176f4b1fcc109d76e1ee4a126c1a0e889ed
MrBabai <48453620+MrBabai@users.noreply.github.com>
2019-04-07 19:00:24 +0300

    Update callend.sh

--- callend.sh
+++ callend.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+apiKey=OurAPI
+idProject=OurID
+
+
+   # $CId - Call ID, $CDur - Call Duration, $CStat - Call Status
+   CId=`echo $CId | tr -d '\r\n\"'`
+   if [ "$CStat" == "ANSWERED" ];
+   then
+	CStat="ANSWER"
+   elif [ "$CStat" == "NO ANSWER" ];
+   then
+	CStat="NOANSWER"
+   else
+	CStat=$CStat
+   fi
+
+   RESULT=`curl --request POST \
+        --url 'https://cloud.roistat.com/api/v1/project/calltracking/call/update?key='$apiKey'&project='$idProject \
+        --header 'content-type: application/json' \
+        --data '{"id":'$CId',"comment":null,"duration":"'$CDur'","status":"'$CStat'"}'`
+   echo $RESULT
